7e40403a1000c4f0deddec46c963895164d08c18
Emmanuel <casianoemmanuel2217@gmail.com>
2023-07-22 00:00:13 +0800

    add index.php

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<?php
+include_once './class/compute.class.php';
+
+$amount=0;
+if( isset( $_GET['submit'] ) ){
+    $amount = filter_var( $_GET['amount'], FILTER_VALIDATE_INT );
+
+    $loan = new Compute( $amount );
+    $loan->computeInstallments();
+    $loanDetails = $loan->getLoadDetails();
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -22,6 +34,7 @@
             <input type="submit" value="COMPUTE" class="btn" name="submit">
         </div>
 
+        <?php if( isset($loanDetails) ): ?>
         <div class="table-container">
             <h2>Your estimated monthly installments are as follows</h2>
 
@@ -29,7 +42,7 @@
                 <div class="loan-table-header">
                     <div class="row1">
                         <div class="amount-title">Loan Amount</div>
-                        <div class="amount-value">100,000</div>
+                        <div class="amount-value"><?=number_format( $amount )?></div>
                     </div>
                     <div class="row2">Term</div>
                     <div class="row2">Monthly Installment</div>
@@ -37,14 +50,14 @@
 
                 <div class="loan-table-body">
                     <div class="loan-table-term">
-                        <div>12</div>
-                        <div>18</div>
-                        <div>24</div>
+                        <?php foreach( $loanDetails as $detail ): ?>
+                        <div><?=$detail['term'];?> Months</div>
+                        <?php endforeach; ?>
                     </div>
                     <div class="loan-table-installments">
-                        <div>9000</div>
-                        <div>6000</div>
-                        <div>5000</div>
+                    <?php foreach( $loanDetails as $detail ): ?>
+                        <div><?=number_format( $detail['value'] );?></div>
+                        <?php endforeach; ?>
                     </div>
                 </div>
                 
@@ -53,10 +66,13 @@
 
         <div class="footer">
             <div class="notice">
-                This is computed at monthly add-on rate of 1.2%
+                This is computed at monthly add-on rate of 1.2%. Actual amounts may vary based on prevailing rates when you apply.
             </div>
             <input type="submit" class="btn btn-center" value="Apply Now" name="apply">
         </div>
+
+        <?php endif; ?>
+
     </form>
 </div>
     
